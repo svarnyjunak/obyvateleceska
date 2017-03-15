e3047f071b99eda89bc0506c7b3123cfc795d054
svarnyjunak <svarnyjunak@gmail.com>
2017-03-15 21:43:03 +0100

    production environment fixed

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/_references.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/_references.js
@@ -1,3 +1,5 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/site.js" />
+/// <reference path="js/chart.js" />
+/// <reference path="js/map.js" />
+/// <reference path="js/search.js" />
 /// <reference path="lib/pixabay-autocompleted/auto-complete.js" />
