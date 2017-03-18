7d1a6b61fa0024830eb8ec1edb44213f62fa65c9
svarnyjunak <svarnyjunak@gmail.com>
2017-03-18 19:22:09 +0100

    about page

--- src/SvarnyJunak.CeskeObce.Web/Controllers/HomeController.cs
+++ src/SvarnyJunak.CeskeObce.Web/Controllers/HomeController.cs
@@ -101,15 +101,6 @@ namespace SvarnyJunak.CeskeObce.Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
