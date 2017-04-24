ff1da729d6f54e119663dff50d0d512322321e90
svarnyjunak <svarnyjunak@gmail.com>
2017-04-24 08:45:23 +0200

    do not use  map when map is not visible

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     var mapElement = document.getElementById("map-canvas");
-    var isMapVisible = mapElement.style.visibility !== "hidden";
+    var isMapVisible = mapElement.style.visibility !== "hidden" && mapElement.offsetWidth > 500; 
     if (isMapVisible) {
         var latitude = Number(document.getElementById("Municipality_Latitude").value);
         var longitude = Number(document.getElementById("Municipality_Longitude").value);
