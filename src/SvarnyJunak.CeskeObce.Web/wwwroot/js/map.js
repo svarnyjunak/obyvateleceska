64f8d2ff41a0e205271dc155338df2994d0dcdd1
Martin Bartoš <svarnyjunak@gmail.com>
2019-04-02 16:27:01 +0200

    google maps removed

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
@@ -1,22 +1,22 @@
-﻿(function () {
-    var mapElement = document.getElementById("map-canvas");
-    var isMapVisible = mapElement.style.visibility !== "hidden" && mapElement.offsetWidth > 500; 
-    if (isMapVisible) {
-        var latitude = Number(document.getElementById("Municipality_Latitude").value);
-        var longitude = Number(document.getElementById("Municipality_Longitude").value);
-        var offset = 0.016408460434895744;
-        var center = new google.maps.LatLng(latitude + offset, longitude);
+﻿//(function () {
+//    var mapElement = document.getElementById("map-canvas");
+//    var isMapVisible = mapElement.style.visibility !== "hidden" && mapElement.offsetWidth > 500; 
+//    if (isMapVisible) {
+//        var latitude = Number(document.getElementById("Municipality_Latitude").value);
+//        var longitude = Number(document.getElementById("Municipality_Longitude").value);
+//        var offset = 0.016408460434895744;
+//        var center = new google.maps.LatLng(latitude + offset, longitude);
 
-        var mapOptions = {
-            center: center,
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            disableDefaultUI: true,
-            draggable: false,
-            zoomControl: false,
-            scrollwheel: false,
-            disableDoubleClickZoom: true 
-        };
-        new google.maps.Map(mapElement, mapOptions);
-    }
-})();
+//        var mapOptions = {
+//            center: center,
+//            zoom: 14,
+//            mapTypeId: google.maps.MapTypeId.HYBRID,
+//            disableDefaultUI: true,
+//            draggable: false,
+//            zoomControl: false,
+//            scrollwheel: false,
+//            disableDoubleClickZoom: true 
+//        };
+//        new google.maps.Map(mapElement, mapOptions);
+//    }
+//})();
