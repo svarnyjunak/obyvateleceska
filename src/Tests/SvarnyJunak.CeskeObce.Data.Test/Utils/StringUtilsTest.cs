d06e753734d8e04b291ae26c03d6bcbbbec23e2f
Martin Bartoš <svarnyjunak@gmail.com>
2020-07-20 15:50:26 +0200

    StringUtils tests added

---
+++ src/Tests/SvarnyJunak.CeskeObce.Data.Test/Utils/StringUtilsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SvarnyJunak.CeskeObce.Data.Utils;
+
+namespace SvarnyJunak.CeskeObce.Data.Test.Utils
+{
+    [TestClass]
+    public class StringUtilsTest
+    {
+        [TestMethod]
+        public void RemoveDiacritics_Test()
+        {
+            var text = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
+            Assert.AreEqual("Prilis zlutoucky kun upel dabelske ody.", text.RemoveDiacritics());
+        }
+
+        [TestMethod]
+        public void HasDiacritics_PositiveTest()
+        {
+            var text = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
+            Assert.IsTrue(text.HasDiacritics());
+        }
+
+        [TestMethod]
+        public void HasDiacritics_NegativeTest()
+        {
+            var text = "Text bez diakritiky.";
+            Assert.IsFalse(text.HasDiacritics());
+        }
+
+        [TestMethod]
+
+        public void CompareWithoutDiacriticsIfNotProvided_PositiveTest()
+        {
+            var text = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
+            Assert.IsTrue("Prilis zlutoucky kun upel dabelske ody.".CompareWithoutDiacriticsIfNotProvided(text));
+        }
+
+        [TestMethod]
+
+        public void CompareWithoutDiacriticsIfNotProvided_NegativeTest()
+        {
+            var text = "Příliš žluťoučký kůň úpěl ďábelské ódy.";
+            Assert.IsFalse(text.CompareWithoutDiacriticsIfNotProvided("Prilis zlutoucky kun upel dabelske ody."));
+        }
+    }
+}
