caefebafbb15ef8affd1b2e717b193ab4efe3ef8
Martin Bartoš <svarnyjunak@gmail.com>
2020-07-20 16:22:25 +0200

    tests added

--- src/Tests/SvarnyJunak.CeskeObce.Data.Test/Repositories/MunicipalityRepositoryTest.cs
+++ src/Tests/SvarnyJunak.CeskeObce.Data.Test/Repositories/MunicipalityRepositoryTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
@@ -27,7 +29,7 @@ namespace SvarnyJunak.CeskeObce.Data.Test.Repositories
                 context.SaveChanges();
 
                 var repository = new MunicipalityRepository(context);
-                var result = repository.Exists(new QueryMunicipalityByCode{Code = "TEST"});
+                var result = repository.Exists(new QueryMunicipalityByCode { Code = "TEST" });
                 Assert.IsTrue(result);
             }
         }
@@ -43,10 +45,73 @@ namespace SvarnyJunak.CeskeObce.Data.Test.Repositories
             }
         }
 
+        [TestMethod]
+        public async Task Save_Test()
+        {
+            using (var dbContext = new CeskeObceDbContext(CreateInMemoryDbContextOptions()))
+            {
+                var repository = new MunicipalityRepository(dbContext);
+
+                var municipality = new Municipality
+                {
+                    MunicipalityId = "TEST",
+                    Name = "Municipality name",
+                    DistrictName = "District name"
+                };
+
+                repository.Save(new[] { municipality });
+
+                Assert.IsNotNull(await dbContext.Municipalities.FindAsync(municipality.MunicipalityId));
+            }
+        }
+
+        [TestMethod]
+        public async Task FindAll_Test()
+        {
+            using (var dbContext = new CeskeObceDbContext(CreateInMemoryDbContextOptions()))
+            {
+                var municipality = new Municipality
+                {
+                    MunicipalityId = "TEST",
+                    Name = "Municipality name",
+                    DistrictName = "District name"
+                };
+
+                await dbContext.Municipalities.AddAsync(municipality);
+                await dbContext.SaveChangesAsync();
+
+                var repository = new MunicipalityRepository(dbContext);
+                var returned = repository.FindAll().Single();
+
+                Assert.AreEqual(municipality, returned);
+            }
+        }
+
+        [TestMethod]
+        public async Task Exists_QueryTest()
+        {
+            using (var context = new CeskeObceDbContext(CreateInMemoryDbContextOptions()))
+            {
+                var municipality = new Municipality
+                {
+                    MunicipalityId = "TEST",
+                    Name = "Municipality name",
+                    DistrictName = "District name"
+                };
+
+                await context.AddAsync(municipality);
+                await context.SaveChangesAsync();
+
+                var repository = new MunicipalityRepository(context);
+                var result = repository.FindAll(new QueryMunicipalityByCode { Code = "TEST" }).Single();
+                Assert.AreEqual(municipality, result);
+            }
+        }
+
         private static DbContextOptions<CeskeObceDbContext> CreateInMemoryDbContextOptions()
         {
             var builder = new DbContextOptionsBuilder<CeskeObceDbContext>();
-            builder.UseInMemoryDatabase("ceske-obce");
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
             var options = builder.Options;
             return options;
         }
---
+++ src/Tests/SvarnyJunak.CeskeObce.Data.Test/Repositories/PopulationFrameRepositoryTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SvarnyJunak.CeskeObce.Data.Entities;
+using SvarnyJunak.CeskeObce.Data.Repositories;
+using SvarnyJunak.CeskeObce.Data.Repositories.Queries;
+
+namespace SvarnyJunak.CeskeObce.Data.Test.Repositories
+{
+    [TestClass]
+    public class PopulationFrameRepositoryTest
+    {
+        [TestMethod]
+        public void Exists_Test()
+        {
+            using (var context = new CeskeObceDbContext(CreateInMemoryDbContextOptions()))
+            {
+                var municipality = new Municipality
+                {
+                    MunicipalityId = "TEST1",
+                    Name = "Municipality name",
+                    DistrictName = "District name"
+                };
+
+                context.Add(municipality);
+
+                var data = new List<PopulationFrame>
+                {
+                    new PopulationFrame
+                    {
+                        Count = 1,
+                        MunicipalityId = municipality.MunicipalityId, 
+                        Year = 2000
+                    }
+                };
+
+                context.AddRange(data);
+                context.SaveChanges();
+
+                var repository = new PopulationFrameRepository(context);
+                var result = repository.Exists(new QueryPopulationFrameByMunicipalityCode
+                {
+                    Code = municipality.MunicipalityId
+                });
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        private static DbContextOptions<CeskeObceDbContext> CreateInMemoryDbContextOptions()
+        {
+            var builder = new DbContextOptionsBuilder<CeskeObceDbContext>();
+            builder.UseInMemoryDatabase("ceske-obce");
+            var options = builder.Options;
+            return options;
+        }
+    }
+}
