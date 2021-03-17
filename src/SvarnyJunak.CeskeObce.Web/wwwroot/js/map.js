40a43b2dda7636bdea464b4b26e6d99012eb3fe3
Martin Bartoš <martin.bartos@inveo.cz>
2021-03-17 19:56:45 +0100

    map is only on right part of page

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
@@ -1,11 +1,10 @@
 ﻿(function () {
     var mapElement = document.getElementById("map-canvas");
-    var isMapVisible = mapElement.style.visibility !== "hidden" && mapElement.offsetWidth > 500;
+    var isMapVisible = mapElement.style.visibility !== "hidden" && mapElement.offsetWidth > 400;
     if (isMapVisible) {
         var latitude = Number(document.getElementById("Municipality_Latitude").value);
         var longitude = Number(document.getElementById("Municipality_Longitude").value);
-        var offset = 0.04;
-        var center = SMap.Coords.fromWGS84(longitude, latitude + (offset));
+        var center = SMap.Coords.fromWGS84(longitude, latitude);
         var m = new SMap(JAK.gel("map-canvas"), center, 13);
         m.addDefaultLayer(SMap.DEF_BASE).enable();
 
