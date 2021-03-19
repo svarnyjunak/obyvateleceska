67023bb7b92f5da2cb038ec85627c959e98523fc
Martin Bartoš <martin.bartos@inveo.cz>
2021-03-19 20:40:07 +0100

    do not cache autocomplete response

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/js/search.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/js/search.js
@@ -1,25 +1,28 @@
 ﻿(function () {
     document.getElementById("municipality-search").focus();
 
+    var xhr;
     new autoComplete({
         minChars: 1,
+        cache: false,
         selector: 'input#municipality-search',
         onSelect: function (e, term, item) {
             var value = item.getAttribute('data-val');
 
             document.querySelector("#municipality-search").value = value;
             document.querySelector("form#select-municipality-form")
-                    .submit();
+                .submit();
         },
         source: function (term, response) {
-            var ajax = new XMLHttpRequest();
-            ajax.open("GET", "/api/municipalities?name=" + encodeURIComponent(term), true);
-            ajax.onload = function () {
-                var data = JSON.parse(ajax.responseText);
+            try { xhr.abort(); } catch (e) { }
+            xhr = new XMLHttpRequest();
+            xhr.open("GET", "/api/municipalities?name=" + encodeURIComponent(term), true);
+            xhr.onload = function () {
+                var data = JSON.parse(xhr.responseText);
                 response(data);
             };
-            ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            ajax.send();
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send();
         }
     });
 })();
