f56913a6ea6a448f7db868115c60cc3897232911
Martin Bartoš <martin.bartos@inveo.cz>
2021-03-19 21:19:51 +0100

    municipality has a marker on map

--- src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
+++ src/SvarnyJunak.CeskeObce.Web/wwwroot/js/map.js
@@ -13,6 +13,17 @@
         var m = new SMap(JAK.gel("map-canvas"), center, 13);
         m.addDefaultLayer(SMap.DEF_BASE).enable();
 
+        var markerLayer = new SMap.Layer.Marker();
+        m.addLayer(markerLayer);
+        markerLayer.enable();
+
+        var markerElement = JAK.mel("div");
+        var img = JAK.mel("img", { src: SMap.CONFIG.img + "/marker/drop-blue.png" });
+        markerElement.appendChild(img);
+
+        var marker = new SMap.Marker(m.getCenter(), null, { url: markerElement });
+        markerLayer.addMarker(marker);
+
         var sync = new SMap.Control.Sync({ bottomSpace: 30 });
         m.addControl(sync);
     };
